1716dce373cd764a79f1779cbe241051f5405ed9
fedpanoz <federicopanozzo@gmail.com>
2024-04-17 19:36:03 +0200

    17.04

--- chapter_06/partial_programs/favorite_languages_0_first_version.py
+++ chapter_06/partial_programs/favorite_languages_0_first_version.py
@@ -8,5 +8,34 @@ favorite_languages = {
     }
 friends = ['gino', 'verbena']
 
+"""for y in favorite_languages.values():
+    print(y)
+print("\n")    
+for y in favorite_languages.keys():
+    print(y)
+
+pippone = favorite_languages.get('pippone', 'No language found')
+print(pippone)"""
+
+for x, y in favorite_languages.items():
+    print(f"{x.title()} favourite language is {y}")
+    
+print("\n")
+
+for x in favorite_languages.keys():
+    print(x)
+print("\n")
+
 for y in favorite_languages.values():
     print(y)
+
+for x, y in favorite_languages.items():
+    if x in friends:
+        print(f"{x.title()} favourite language is {y}")
+        
+             
+print(favorite_languages.keys())
+print(favorite_languages.values())
+print(type(favorite_languages.keys()))
+print(type(favorite_languages.values()))
+      
