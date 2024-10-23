ca451ac93d5c56aa8fd19859e95188989455f03d
fedpanoz <federicopanozzo@gmail.com>
2024-10-23 09:57:11 +0200

    23.10

--- chapter_10/reading_from_a_file/mia_prova_Path.py
+++ chapter_10/reading_from_a_file/mia_prova_Path.py
@@ -1,4 +1,4 @@
 from pathlib import Path
-path = Path('C:/Users/zizza/Documents/GitHub/pcc_3e/chapter_10/reading_from_a_file/prova1.txt')
-contents = path.read_text()
-print(contents)
+oggetto = Path('C:/Users/zizza/Documents/GitHub/pcc_3e/chapter_10/reading_from_a_file/prova1.txt')
+contenuto = oggetto.read_text()
+print(contenuto)
