48c2852bae07fd8d639afbaee7fb1d398f9799c3
fedpanoz <federicopanozzo@gmail.com>
2024-10-26 10:00:19 +0200

    26.10

--- chapter_10/reading_from_a_file/mia_prova_Path.py
+++ chapter_10/reading_from_a_file/mia_prova_Path.py
@@ -1,4 +1,11 @@
 from pathlib import Path
 oggetto = Path('C:/Users/zizza/Documents/GitHub/pcc_3e/chapter_10/reading_from_a_file/prova1.txt')
 contenuto = oggetto.read_text()
-print(contenuto)
+linee = contenuto.splitlines()
+print(linee)
+print(type(linee))
+
+for linea in linee:
+    print(linea)
+    
+print(type(oggetto))
