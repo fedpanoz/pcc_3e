a1b8893ab212cd156430cc16708d4d3770c6e080
fedpanoz <federicopanozzo@gmail.com>
2024-10-27 14:52:57 +0100

    27.10

---
+++ chapter_10/storing_data/prova_jason_user.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+import json
+
+nome = input('dimmi il tuo nome:\n')
+oggetto = Path('C:/Users/zizza/Documents/GitHub/pcc_3e/chapter_10/storing_data/utenza.json')
+contenuto = json.dumps(nome)
+oggetto.write_text(contenuto)
+
+print(f'ben tornato {nome}')
