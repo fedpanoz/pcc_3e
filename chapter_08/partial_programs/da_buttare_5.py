9e2bb4c727dab5c5b0a0546d08dd72c540581b7d
fedpanoz <federicopanozzo@gmail.com>
2024-11-08 00:23:43 +0100

    08.11

--- chapter_08/partial_programs/da_buttare_5.py
+++ chapter_08/partial_programs/da_buttare_5.py
@@ -1,5 +1,6 @@
-def printing_pizzas(*toppings):
-    print(toppings)
-
-printing_pizzas('peppe', 'formaggio', 'acciughe')
-print(type(printing_pizzas()))
+def libro_favorito(titolo='La Luna e il falo' , autore='Bergonzoni'):
+    print("Il mio libro preferito è", titolo)
+    print("L'autore è", autore)
+    
+libro_favorito('Il vecchio e il mare', 'Hemingway')
+libro_favorito()
